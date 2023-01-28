4cdda1b17fa4bd179413cc9cbb8ebba80d30f346
alex_ <alex_kr@abv.bg>
2023-01-28 13:44:10 +0200

    ExercisesTests update

--- src/test/java/ExersisesTest/ExercisesTest.java
+++ src/test/java/ExersisesTest/ExercisesTest.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.time.Instant;
 
 public class ExercisesTest {
+
     @Test
     public void testLogin() {
         WebDriverManager.chromedriver().setup();
